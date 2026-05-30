cadb1135576b4a425b9e0d95db3a9e329f658809
jywoooo <wnduddk1119@naver.com>
2026-05-30 23:54:03 +0900

    카카오톡 공유하기 버튼 추가

--- src/component/shareButton/index.tsx
+++ src/component/shareButton/index.tsx
@@ -1,9 +1,9 @@
 import {
   BRIDE_FULLNAME,
   GROOM_FULLNAME,
+  KMAP_PLACE_ID,
   LOCATION,
-  SHARE_ADDRESS,
-  SHARE_ADDRESS_TITLE,
+  LOCATION_ADDRESS,
   WEDDING_DATE,
   WEDDING_DATE_FORMAT,
 } from "../../const"
@@ -12,6 +12,9 @@ import { LazyDiv } from "../lazyDiv"
 import { useKakao } from "../store"
 
 const baseUrl = import.meta.env.BASE_URL
+
+const getShareUrl = () =>
+  window.location.protocol + "//" + window.location.host + baseUrl
 
 /**
  * 카카오톡으로 초대장을 공유할 수 있는 버튼 컴포넌트입니다.
@@ -30,52 +33,49 @@ export const ShareButton = () => {
             return
           }
 
-          // 카카오톡 공유 전송 (위치 기반 템플릿 사용)
+          const shareUrl = getShareUrl()
+          const invitationLink = { mobileWebUrl: shareUrl, webUrl: shareUrl }
+          const mapUrl = `https://map.kakao.com/link/map/${KMAP_PLACE_ID}`
+          const locationLink = { mobileWebUrl: mapUrl, webUrl: mapUrl }
+
+          // feed 템플릿: buttons 최대 2개 (location 템플릿은 위치 보기 버튼이 고정·커스텀 불가)
           kakao.Share.sendDefault({
-            objectType: "location",
-            address: SHARE_ADDRESS,
-            addressTitle: SHARE_ADDRESS_TITLE,
+            objectType: "feed",
             content: {
               title: `${GROOM_FULLNAME} ❤️ ${BRIDE_FULLNAME}의 결혼식에 초대합니다.`,
-              description:
-                WEDDING_DATE.format(WEDDING_DATE_FORMAT) + "\n" + LOCATION,
-              imageUrl:
-                window.location.protocol +
-                "//" +
-                window.location.host +
-                baseUrl +
-                "/preview_image.png",
-              link: {
-                mobileWebUrl:
-                  window.location.protocol +
-                  "//" +
-                  window.location.host +
-                  baseUrl,
-                webUrl:
-                  window.location.protocol +
-                  "//" +
-                  window.location.host +
-                  baseUrl,
-              },
+              description: [
+                WEDDING_DATE.format(WEDDING_DATE_FORMAT),
+                LOCATION,
+                LOCATION_ADDRESS,
+              ].join("\n"),
+              imageUrl: shareUrl + "preview_image.png",
+              link: invitationLink,
             },
             buttons: [
-              {
-                title: "초대장 보기",
-                link: {
-                  mobileWebUrl:
-                    window.location.protocol +
-                    "//" +
-                    window.location.host +
-                    baseUrl,
-                  webUrl:
-                    window.location.protocol +
-                    "//" +
-                    window.location.host +
-                    baseUrl,
-                },
-              },
+              { title: "초대장 보기", link: invitationLink },
+              { title: "위치 보기", link: locationLink },
             ],
           })
+
+          // --- 기존: location 템플릿 (위치 보기 버튼 고정, buttons 커스텀 불가) ---
+          // kakao.Share.sendDefault({
+          //   objectType: "location",
+          //   address: LOCATION,
+          //   addressTitle: LOCATION,
+          //   content: {
+          //     title: `${GROOM_FULLNAME} ❤️ ${BRIDE_FULLNAME}의 결혼식에 초대합니다.`,
+          //     description:
+          //       WEDDING_DATE.format(WEDDING_DATE_FORMAT) + "\n" + LOCATION,
+          //     imageUrl: shareUrl + "preview_image.png",
+          //     link: invitationLink,
+          //   },
+          //   buttons: [
+          //     {
+          //       title: "초대장 보기",
+          //       link: invitationLink,
+          //     },
+          //   ],
+          // })
         }}
       >
         <img src={ktalkIcon} alt="ktalk-icon" /> 카카오톡으로 공유하기
