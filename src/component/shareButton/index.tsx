01baec86930b854e0df37660cb19ba7193447f52
jywoooo <wnduddk1119@naver.com>
2026-05-31 00:05:22 +0900

    카카오톡 공유하기 버튼 추가 2트

--- src/component/shareButton/index.tsx
+++ src/component/shareButton/index.tsx
@@ -1,7 +1,6 @@
 import {
   BRIDE_FULLNAME,
   GROOM_FULLNAME,
-  KMAP_PLACE_ID,
   LOCATION,
   LOCATION_ADDRESS,
   WEDDING_DATE,
@@ -13,8 +12,8 @@ import { useKakao } from "../store"
 
 const baseUrl = import.meta.env.BASE_URL
 
-const getShareUrl = () =>
-  window.location.protocol + "//" + window.location.host + baseUrl
+const getShareUrl = (path = "") =>
+  new URL(path, window.location.origin + baseUrl).href
 
 /**
  * 카카오톡으로 초대장을 공유할 수 있는 버튼 컴포넌트입니다.
@@ -33,31 +32,51 @@ export const ShareButton = () => {
             return
           }
 
-          const shareUrl = getShareUrl()
-          const invitationLink = { mobileWebUrl: shareUrl, webUrl: shareUrl }
-          const mapUrl = `https://map.kakao.com/link/map/${KMAP_PLACE_ID}`
-          const locationLink = { mobileWebUrl: mapUrl, webUrl: mapUrl }
+          const invitationLink = {
+            mobileWebUrl: getShareUrl(),
+            webUrl: getShareUrl(),
+          }
 
-          // feed 템플릿: buttons 최대 2개 (location 템플릿은 위치 보기 버튼이 고정·커스텀 불가)
+          // location: buttonTitle(초대장) + 고정 "위치 보기" 2개 버튼
+          // feed의 buttons는 링크 도메인이 카카오 앱에 등록되어야 표시됨 (미등록 시 버튼 없음)
           kakao.Share.sendDefault({
-            objectType: "feed",
+            objectType: "location",
+            address: LOCATION_ADDRESS,
+            addressTitle: LOCATION,
             content: {
               title: `${GROOM_FULLNAME} ❤️ ${BRIDE_FULLNAME}의 결혼식에 초대합니다.`,
-              description: [
-                WEDDING_DATE.format(WEDDING_DATE_FORMAT),
-                LOCATION,
-                LOCATION_ADDRESS,
-              ].join("\n"),
-              imageUrl: shareUrl + "preview_image.png",
+              description:
+                WEDDING_DATE.format(WEDDING_DATE_FORMAT) + "\n" + LOCATION,
+              imageUrl: getShareUrl("preview_image.png"),
               link: invitationLink,
             },
-            buttons: [
-              { title: "초대장 보기", link: invitationLink },
-              { title: "위치 보기", link: locationLink },
-            ],
+            buttonTitle: "초대장 보기",
           })
 
-          // --- 기존: location 템플릿 (위치 보기 버튼 고정, buttons 커스텀 불가) ---
+          // --- feed 템플릿 (버튼 2개 직접 지정, 도메인 등록 필요) ---
+          // const mapLink = {
+          //   mobileWebUrl: `https://map.kakao.com/link/map/${KMAP_PLACE_ID}`,
+          //   webUrl: `https://map.kakao.com/link/map/${KMAP_PLACE_ID}`,
+          // }
+          // kakao.Share.sendDefault({
+          //   objectType: "feed",
+          //   content: {
+          //     title: `${GROOM_FULLNAME} ❤️ ${BRIDE_FULLNAME}의 결혼식에 초대합니다.`,
+          //     description: [
+          //       WEDDING_DATE.format(WEDDING_DATE_FORMAT),
+          //       LOCATION,
+          //       LOCATION_ADDRESS,
+          //     ].join("\n"),
+          //     imageUrl: getShareUrl("preview_image.png"),
+          //     link: invitationLink,
+          //   },
+          //   buttons: [
+          //     { title: "초대장 보기", link: invitationLink },
+          //     { title: "위치 보기", link: mapLink },
+          //   ],
+          // })
+
+          // --- 기존: location + buttons (커스텀 버튼이 무시되고 위치 보기만 보이던 설정) ---
           // kakao.Share.sendDefault({
           //   objectType: "location",
           //   address: LOCATION,
@@ -66,7 +85,7 @@ export const ShareButton = () => {
           //     title: `${GROOM_FULLNAME} ❤️ ${BRIDE_FULLNAME}의 결혼식에 초대합니다.`,
           //     description:
           //       WEDDING_DATE.format(WEDDING_DATE_FORMAT) + "\n" + LOCATION,
-          //     imageUrl: shareUrl + "preview_image.png",
+          //     imageUrl: getShareUrl("preview_image.png"),
           //     link: invitationLink,
           //   },
           //   buttons: [
